636c37258394659fc75f67dcc299cb17f1d3bd8c
tuba0809 <tubasa8342849@yahoo.co.jp>
2021-12-20 09:21:33 +0000

    コメント

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,20 +1,20 @@
 class PostImagesController < ApplicationController
-  
+  #記事投稿
   def new
      @post_image = PostImage.new
   end
-
+#記事保存
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
   end
-
+#記事一覧
   def index
     @post_images = PostImage.all
   end
-
+#記事詳細
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
