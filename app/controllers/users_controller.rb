d5192a82ccbf844f93af9c703d61a22ea18cdf3c
tuba0809 <tubasa8342849@yahoo.co.jp>
2021-12-28 23:59:59 +0900

    COMMIT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  #ユーザ
+  #ユーザ一覧
   def show
      @user = User.find(params[:id])
      @post_images = @user.post_images.page(params[:page]).reverse_order
   end
-  
+  #ユーザ
   def edit
     @user = User.find(params[:id])
   end
